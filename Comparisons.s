9e1e82c39a37defe77e373fe8dbac618da5402ff
KILYAV <88192934+KILYAV@users.noreply.github.com>
2023-11-06 22:45:55 +0300

    Update and rename Compare.s to Comparisons.s

--- Comparisons.s
+++ Comparisons.s
@@ -1,4 +1,4 @@
-//	friend bool operator== (_float, _float);
+// friend bool operator== (_float, _float);
 .global _Zeq6_floatS_
 _Zeq6_floatS_:
 	cmp r0, r1
@@ -13,7 +13,7 @@ _Zeq6_floatS_:
 	movls r0, #1
 	bx lr
 
-//	friend bool operator!= (_float, _float);
+// friend bool operator!= (_float, _float);
 .global _Zne6_floatS_
 _Zne6_floatS_:
 	mov r2, #0xff000000
