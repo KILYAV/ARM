02d2d1069c8a48dc3ed71556bc3f7cbdddafed30
KILYAV <88192934+KILYAV@users.noreply.github.com>
2023-11-06 22:35:15 +0300

    Update Static_Function.s

--- Static_Function.s
+++ Static_Function.s
@@ -1,19 +1,23 @@
+//	static _float Inf();
 .global _ZN6_float3InfEv
 _ZN6_float3InfEv:
 	mov r0, #0x7f800000
 	bx lr
 
+//	static _float SNan();
 .global _ZN6_float4SNanEv
 _ZN6_float4SNanEv:
 	mov r0, #0x7f800000
 	orr r0, #1
 	bx lr
 
+//	static _float QNan();
 .global _ZN6_float4QNanEv
 _ZN6_float4QNanEv:
 	eor r0, r0
 	movt r0, #0x7fc0
 
+//	static _float Hex(uint32_t);
 .global _ZN6_float3HexEm
 _ZN6_float3HexEm:
 	bx lr
