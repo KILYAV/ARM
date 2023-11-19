73bf925247bc211fd4835de8980677e083c4c208
KILYAV <88192934+KILYAV@users.noreply.github.com>
2023-11-19 19:55:03 +0300

    Update Cmp.s

--- Cmp.s
+++ Cmp.s
@@ -1,3 +1,33 @@
+// friend bool operator== (_float, _float);
+.global _Zeq6_floatS_
+_Zeq6_floatS_:
+	cmp r0, r1
+	mov r0, #0
+	it ne
+	bxne lr
+
+	mov r0, #1
+	movt r0, #0xff00
+	rsb r0, r0, r1, lsl #1
+	lsr r0, #31
+	bx lr
+
+// friend bool operator!= (_float, _float);
+.global _Zne6_floatS_
+_Zne6_floatS_:
+	mov r2, #0xff000000
+	cmp r2, r1, lsl #1
+	ittt lo
+	cmplo r2, r1, lsl #1
+	movlo r0, #1
+	bxlo lr
+
+	mov r0, #1
+	cmp r0, r1
+	it eq
+	eoreq r0, r0
+	bx lr
+
 // friend bool operator<= (_float, _float);
 .global _Zle6_floatS_
 _Zle6_floatS_:
