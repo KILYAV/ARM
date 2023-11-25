3d8c0da0df3c8e7b531659af6182607fa0fb703b
KILYAV <88192934+KILYAV@users.noreply.github.com>
2023-11-25 16:44:12 +0300

    Update Cmp.s

--- Cmp.s
+++ Cmp.s
@@ -16,16 +16,13 @@ _Zeq6_floatS_:
 .global _Zne6_floatS_
 _Zne6_floatS_:
 	mov r2, #0xff000000
-	cmp r2, r1, lsl #1
+	cmp r2, r0, lsl #1
 	ittt lo
 	cmplo r2, r1, lsl #1
 	movlo r0, #1
 	bxlo lr
 
-	mov r0, #1
-	cmp r0, r1
-	it eq
-	eoreq r0, r0
+	sub r0, r1
 	bx lr
 
 // friend bool operator<= (_float, _float);
