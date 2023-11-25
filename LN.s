18dd17edae77bd4ed64a9341bd9a453aa0c44d76
KILYAV <88192934+KILYAV@users.noreply.github.com>
2023-11-25 22:39:48 +0300

    Update LN.s

--- LN.s
+++ LN.s
@@ -1,11 +1,20 @@
-.global _start
-_start:
-//------------------
-	ldr r0, _data
-	
-//------------------
-	lsr r3, r0, #23
-	eor r0, r3, lsl #23
+// 	_float _float::Ln();
+.global _ZN6_float2LnEv
+_ZN6_float2LnEv:
+	cmp r0, #0x80000000
+	bhs _ln_nan_exit
+	tst r0, r0
+	beq _ln_nan_exit
+
+	cmp r0, #0x7f800000
+	it hs
+	bxhs lr
+
+	lsrs r3, r0, #23
+	beq _ln_neg_inf_exit
+
+	eors r0, r0, r3, lsl #23
+	beq _ln_exp_exit
 
 // (x - 1) / (x + 1)
 	lsls r0, #9
@@ -16,6 +25,7 @@ _start:
 	asr r0, #1
 	lsr r1, r0, #8
 	orr r1, #0x01000000
+	itt mi
 	mvnmi r0, r0
 	addmi r0, #1
 
@@ -23,39 +33,34 @@ _start:
 	lsl r0, r4
 
 	cmp r0, r1, lsl #7
+	ite pl
 	lsrpl r0, #1
 	addmi r4, #1
 	eor r3, r3
-	
-// temp code	
-	lsr r0, #6
+
 _ln_div:
-	subs r0, r1
-	addmi r0, r1
-	lsl r0, #1
+	udiv r2, r0, r1
+	orr r3, r2, r3, lsl #7
+	mul r2, r2, r1
+	sub r0, r2
+	rsbs r2, r1, r0, lsl #1
+	it mi
+	lslmi r2, r0, #1
+	lsl r0, r2, #7
 	adcs r3, r3
 	bpl _ln_div
 
-//	udiv r2, r0, r1
-//	orr r3, r2, r3, lsl #7
-//	mul r2, r2, r1
-//	sub r0, r2
-//	rsbs r2, r1, r0, lsl #1
-//	lslmi r2, r0, #1
-//	lsl r0, r2, #7
-//	adcs r3, r3
-//	bpl _ln_div
-
 // argument & argument^2
 	push {r3-r5}
-	umulls r1, r0, r3, r3
+	ldr r1, _ln_cons + 16
+	cmp r4, #15
+	bhs _ln_add_mul
+
+	umull r1, r0, r3, r3
 	clz r2, r0
 	lsl r0, r2
 	add r4, r2, r4, lsl #1
-	orrpl r0, r1, lsr #31
-	cmp r4, #30
-	ldrhs r1, _ln_cons + 16
-	bhs _ln_add_mul
+//	orrpl r0, r0, r1, lsr #31
 
 	adr r6, _ln_cons
 	ldr r5, [r6], #4
@@ -76,21 +81,24 @@ _ln_add_mul:
 	umull r1, r0, r1, r0
 	subs r4, #0x7f
 	lsr r5, r4, #31
+	itt mi
 	mvnmi r4, r4
 	addmi r4, #1
 
 	clz r6, r4
 	lsl r4, r6
 	rsb r6, #32
+	ittee ne
 	lsrne r0, r6
 	lsrne r0, r2
 	moveq r5, r3
 	subeq r6, r2
-	
+
 	eors r3, r5
+	ite ne
 	subne r0, r4, r0
 	addeq r0, r4, r0
-	
+
 	ldr r1, _ln_cons + 20
 	umull r1, r0, r1, r0
 	clz r1, r0
@@ -99,19 +107,29 @@ _ln_add_mul:
  	sub r6, #1
 	asr r0, #6
 	lsr r0, #2
-	add r0, r6, lsl #23
-	orr r0, r5, lsl #31 
+	add r0, r0, r6, lsl #23
+	orr r0, r0, r5, lsl #31
 	pop {r4-r6}
-	
-_data:
-	.word 0x3f800001
+	bx lr
 
+.align(2)
 _ln_cons:
 	.word 0x00020001
-	
 	.word 0xe0cecb00
 	.word 0x938ea900
 	.word 0xf638ab00
 	.word 0xb8aa3b00
-
 	.word 0xb17217f8
+
+_ln_exp_exit:
+	bx lr
+
+_ln_neg_inf_exit:
+	mov r0, #0
+	movt r0, #0xff80
+	bx lr
+
+_ln_nan_exit:
+	mov r0, #0
+	movt r0, #0x7fc0
+	bx lr
