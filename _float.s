641a3d4837414f8c6656e8bd995f4d341e4eb45a
KILYAV <88192934+KILYAV@users.noreply.github.com>
2023-11-26 22:16:56 +0300

    Update _float.s

--- _float.s
+++ _float.s
@@ -1,11 +1,13 @@
-// _float(float f);
+// _float(float);
 .global _ZN6_floatC1Ef
 _ZN6_floatC1Ef:
+// _float::_float(float f) : number(f) {};
 	str r1, [r0]
 	bx lr
 
 // operator float ();
 .global _ZN6_floatcvfEv
 _ZN6_floatcvfEv:
+// _float::operator float() { return number; };
 	ldr r0, [r0]
 	bx lr
